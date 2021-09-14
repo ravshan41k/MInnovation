0eb9c5d701017be1b83a397598fac16086c62b8c
Ravshan <rabbanoyev98@mail.ru>
2021-09-14 13:24:37 +0500

    add classes

---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MInnovation.Models
+{
+    public class Category
+    {    
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  byte picture { get; set; }
+    }
+}
